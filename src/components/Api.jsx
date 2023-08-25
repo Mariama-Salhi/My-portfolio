22a83a624c6d393a1ead9c6c923375ac5d5e577d
Mariama <merysalhi32@gmail.com>
2023-08-25 11:27:06 +0200

    s

--- src/components/Api.jsx
+++ src/components/Api.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./Api.css";
 import githubIcon from "/images/github-icon.png";
 import linkedinIcon from "/images/linkedin-icon.png";
@@ -6,50 +6,60 @@ import instagramIcon from "/images/instagram-icon.png";
 import myPhoto from "/images/my-photo.png";
 import myPhoto2 from "/images/my-photo2.png";
 
-
 function Api() {
-  const [aboutMeData, setAboutMeData] = useState(null);
-
-  useEffect(() => {
-    fetch("https://react-2dc9c-default-rtdb.firebaseio.com/About%20me.json")
-      .then((response) => response.json())
-      .then((data) => setAboutMeData(data))
-      .catch((error) => console.log(error));
-  }, []);
-
+  const aboutMeData = {
+    description:
+    "Passionate tech enthusiast and creative developer. I blend technical expertise with artistic vision to bring digital experiences to life. My creativity shines in designing intuitive, visually stunning interfaces that deliver exceptional user experiences. Transforming ideas into reality through code is my forte. Need a fresh perspective? Let's create together! Im ready  ",
+    socialMedia: [
+      { name: "GitHub" },
+      { name: "LinkedIn" },
+      { name: "Instagram" },
+    ],
+  };
+  
   return (
-<div className="api-container">
-  {aboutMeData && (
-    <>
-    <img  class="left-image" src={myPhoto} alt="My Photo" />
-    <img  class="right-image" src={myPhoto2} alt="My Photo" />
-    
-    <h2>About Me</h2>
-    
-    
-     {/* <h2>{aboutMeData.name}</h2> */}
-      
-      <p>{aboutMeData.description}</p>
-      <ul className="social-media-list">
-        {aboutMeData.socialMedia.map((social) => (
-          <li key={social.name}>
-            {social.name === "GitHub" && (
-              <img src={githubIcon} alt={social.name} className="social-media-icon"/>
-            )}
-            {social.name === "LinkedIn" && (
-              <img src={linkedinIcon} alt={social.name} className="social-media-icon"/>
-            )}
-            {social.name === "Instagram" && (
-              <img src={instagramIcon} alt={social.name} className="social-media-icon"/>
-            )}
-          </li>
-        ))}
-         
-      </ul>
-    </>
-  )}
-</div>
-);
+    <div className="api-container">
+      <>
+        <img class="left-image" src={myPhoto} alt="My Photo" />
+        <img class="right-image" src={myPhoto2} alt="My Photo" />
+
+        <h2>About Me</h2>
+        
+
+        <p>{aboutMeData.description}</p>
+        <ul className="social-media-list">
+          {aboutMeData.socialMedia.map((social) => (
+            <li key={social.name}>
+              {social.name === "GitHub" && (
+                <img
+                  src={githubIcon}
+                  alt={social.name}
+                  className="social-media-icon"
+                />
+              )}
+              {social.name === "LinkedIn" && (
+                <img
+                  src={linkedinIcon}
+                  alt={social.name}
+                  className="social-media-icon"
+                />
+              )}
+              {social.name === "Instagram" && (
+                <img
+                  src={instagramIcon}
+                  alt={social.name}
+                  className="social-media-icon"
+                />
+              )}
+            </li>
+          ))}
+        </ul>
+      </>
+    </div>
+  );
 }
 
 export default Api;
+
+
+
